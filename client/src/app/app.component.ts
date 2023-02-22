ae5310b0c35c61810f98b6aa25511f8445550eca
Ahmed Mokhtar <55771788+Ahmed7Mokhtar@users.noreply.github.com>
2023-02-22 05:36:44 +0200

    getting products from API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
+import { IPagination } from './Models/pagination';
+import { IProduct } from './Models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products!:IProduct[];
+  
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._http.get<IPagination>('https://localhost:7246/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
