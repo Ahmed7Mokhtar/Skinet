8a5dd6957b34b8d997b71ac4e25af0eca11be0f7
Ahmed Mokhtar <55771788+Ahmed7Mokhtar@users.noreply.github.com>
2023-02-28 11:34:43 +0200

    adding authorization to client side

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,26 +4,35 @@ import {PaginationModule} from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './Components/paging-header/paging-header.component';
 import { PagerComponent } from './Components/pager/pager.component'
 import {CarouselModule} from 'ngx-bootstrap/carousel';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { OrderTotalsComponent } from './Components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './Components/text-input/text-input.component';
 
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ReactiveFormsModule
   ],
   exports : [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
-    OrderTotalsComponent
+    BsDropdownModule,
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
